3b318a37429dabd294a521a5c66da52a5ff60cfc
Jacob Reed <jacob.russell.reed@gmail.com>
2021-11-03 16:03:28 -0400

    update

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@ Set-ExecutionPolicy AllSigned
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install -y steam-client nvidia-display-driver streamlabs-obs epicgameslauncher vscode virtualbox streamdeck icue googlechrome sharex python spotify 7zip nvm powertoys vlc neovim  msiafterburner git firacode choco spicetify-cli discord gh
+choco install -y steam-client nvidia-display-driver streamlabs-obs epicgameslauncher vscode virtualbox streamdeck icue googlechrome sharex python spotify 7zip nvm powertoys vlc neovim  msiafterburner git firacode choco spicetify-cli discord gh lazygit pandoc ripgrep mingw
